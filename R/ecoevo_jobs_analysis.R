cde3bfdbb38171af958ddb9a52fd826b71a1cba0
Mike Blazanin <mikeblazanin@gmail.com>
2023-07-05 14:12:07 -0400

    Git thinks a bunch of lines changed but they didn't. All I did was rename "Institution name" col in output to "Matched institution name"

--- R/ecoevo_jobs_analysis.R
+++ R/ecoevo_jobs_analysis.R
@@ -15,7 +15,7 @@ jobs <- lapply(jobs, function(x){return(x[, 1:(which(colnames(x) == "Mod Flag")-
 jobs <- lapply(jobs, FUN = function(x) {return(x[!is.na(x$Timestamp), ])})
 
 #Read in Carnegie data sheets
-carnegie_val <- 
+carnegie_val <-
   readxl::read_excel("./data-raw/CCIHE2021-PublicData.xlsx", sheet = "Values")
 colnames(carnegie_val) <- c("Variable", "Label", "Value", "Value_Label")
 carnegie_val <- carnegie_val[!is.na(carnegie_val$Value_Label), ]
@@ -44,7 +44,7 @@ carnegie_val <- dplyr::mutate(
                        Value_Label))
 
 
-carnegie_dat <- 
+carnegie_dat <-
   readxl::read_excel("./data-raw/CCIHE2021-PublicData.xlsx", sheet = "Data")
 carnegie_dat <- dplyr::select(carnegie_dat,
                               !c(basic2000, basic2005, basic2010,
@@ -62,21 +62,21 @@ aliases <- read.csv("./data-raw/aliases.csv", fileEncoding = "UTF-8")
 #Convert numeric codes into meaningful labels
 for(i in 1:ncol(carnegie_dat)) {
   col <- colnames(carnegie_dat)[i]
-  if(any(grepl(pattern = paste0("^", col, "$"), carnegie_val$Variable, 
+  if(any(grepl(pattern = paste0("^", col, "$"), carnegie_val$Variable,
                ignore.case = TRUE))) {
-    temp <- carnegie_val[grep(pattern = paste0("^", col, "$"), carnegie_val$Variable, 
+    temp <- carnegie_val[grep(pattern = paste0("^", col, "$"), carnegie_val$Variable,
                               ignore.case = TRUE), ]
     carnegie_dat[, i] <- temp$Value_Label[match(carnegie_dat[, i], temp$Value)]
   }
 }
 
 #Update column names to be long-form understandable labels
-colnames(carnegie_dat) <- 
+colnames(carnegie_dat) <-
   sapply(colnames(carnegie_dat),
          mytable = carnegie_var$Variable,
          mylabels = carnegie_var$Label,
          function(clnm, mytable, mylabels) {
-           mylabels[grep(pattern = paste0("^", clnm, "$"), 
+           mylabels[grep(pattern = paste0("^", clnm, "$"),
                          x = mytable, ignore.case = TRUE)]})
 
 #Match institution names
@@ -85,7 +85,7 @@ find_matches <- function(jobs, carnegie, aliases) {
   #Jobs should have columns named "Institution" and "Location"
   #Carnegie should have column named "Institution name"
   jobs <- cbind(jobs, NA, NA, NA)
-  colnames(jobs)[(ncol(jobs)-2):ncol(jobs)] <- 
+  colnames(jobs)[(ncol(jobs)-2):ncol(jobs)] <-
     c("Institution name", "partial_matched", "State abbreviation")
   for(i in 1:nrow(jobs)) {
     myinst <- jobs$Institution[i]
@@ -109,21 +109,21 @@ find_matches <- function(jobs, carnegie, aliases) {
       jobs$partial_matched[i] <- FALSE
     }
   }
-  
+
   #For rows where plain matching failed, do partial matching
   #Manually chosen rules include:
   #   Treat "The" from beginning of Carnegie as optional
   #   Treat " at " anywhere in Carnegie as optional
-  #   Carnegie uses "-" with no spaces, but ecoevo sometimes 
+  #   Carnegie uses "-" with no spaces, but ecoevo sometimes
   #     uses ", " or " " or " - "
   #Change all "St" and "St." to be "Saint"
-  jobs_inst_mod <- 
+  jobs_inst_mod <-
     trimws(gsub("^The |, | at |-| - ", " ",
                 gsub("St |St\\. ", "Saint ", jobs$Institution)))
-  carnegie_inst_mod <- 
+  carnegie_inst_mod <-
     trimws(gsub("^The |, | at |-| - ", " ",
                 gsub("St |St\\. ", "Saint ", carnegie$`Institution name`)))
-  
+
   for(i in which(is.na(jobs$`Institution name`))) {
     myinst <- jobs_inst_mod[i]
     matches <- grep(paste0("^", myinst, "$"), carnegie_inst_mod)
@@ -143,14 +143,14 @@ find_matches <- function(jobs, carnegie, aliases) {
   #For rows where plain and partial matching failed, do matching using
   # manually-curated list of aliases
   unmatched_names <- unique(jobs$Institution[is.na(jobs$`Institution name`)])
-  
+
   #original aliases.csv file created by running
-  # data.frame("ecoevo_names" = unmatched_names, 
+  # data.frame("ecoevo_names" = unmatched_names,
   #            "carnegie_names" = NA, "checked" = "N"))
-  
+
   if(any(!unmatched_names %in% aliases$ecoevo_names)) {
     which_msng <- which(!unmatched_names %in% aliases$ecoevo_names)
-    aliases <- 
+    aliases <-
       rbind(aliases,
             data.frame("ecoevo_names" = unmatched_names[which_msng],
                        "carnegie_names" = NA, "checked" = "N"))
@@ -161,7 +161,7 @@ find_matches <- function(jobs, carnegie, aliases) {
   myrows <- which(is.na(jobs$`Institution name`))
   jobs$`Institution name`[myrows] <-
     aliases$carnegie_names[match(jobs$Institution[myrows], aliases$ecoevo_names)]
-  
+
   jobs$partial_matched[myrows][!is.na(jobs$`Institution name`[myrows])] <- TRUE
 
   #Return results
@@ -173,12 +173,12 @@ jobs1 <- find_matches(jobs = jobs[[1]], carnegie = carnegie_dat,
 jobs2 <- find_matches(jobs = jobs[[2]], carnegie = carnegie_dat,
                       aliases = aliases)
 
-if(nrow(jobs1$aliases) > nrow(aliases)) {                            
-  write.csv(x = jobs1$aliases, file = "./data-raw/aliases_new1.csv", 
+if(nrow(jobs1$aliases) > nrow(aliases)) {
+  write.csv(x = jobs1$aliases, file = "./data-raw/aliases_new1.csv",
             row.names = FALSE, fileEncoding = "UTF-8")
 }
 if(nrow(jobs2$aliases) > nrow(aliases)) {
-  write.csv(x = jobs2$aliases, file = "./data-raw/aliases_new2.csv", 
+  write.csv(x = jobs2$aliases, file = "./data-raw/aliases_new2.csv",
             row.names = FALSE, fileEncoding = "UTF-8")
 }
 
@@ -188,6 +188,9 @@ if(nrow(jobs2$aliases) > nrow(aliases)) {
 #Join carnegie_dat and jobs using matched institution name
 jobs1 <- dplyr::left_join(x = jobs1$jobs, y = carnegie_dat)
 jobs2 <- dplyr::left_join(x = jobs2$jobs, y = carnegie_dat)
+
+jobs1 <- rename(jobs1, "Matched institution name" = "Institution name")
+jobs2 <- rename(jobs2, "Matched institution name" = "Institution name")
 
 write.csv(jobs1, "./data-raw/ecoevojobsR_faculty.csv", row.names = FALSE)
 write.csv(jobs2, "./data-raw/ecoevojobsR_postdoc.csv", row.names = FALSE)
